14e4938277eeea71f2e96ead09a2fa95cfe51d9f
w4ngzhen <w4ngzhen@hotmail.com>
2022-05-27 14:51:25 +0800

    add: add style codes.

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
+// 引入less样式
+import './index.less';
 
 interface ButtonProps {
 }
 
 const Button: React.FC<ButtonProps> = (props) => {
     const {children, ...rest} = props;
-    return <button {...rest} >{children}</button>
+    // 使用my-button样式
+    return <button {...rest} className='my-button'>{children}</button>
 }
 
 export default Button;
