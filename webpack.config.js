ce87e91dc111c3e5b11b5e2583d9c6202e02c15a
kivet <wanghuixiang@druid.tech>
2024-03-13 20:29:36 +0800

    update: build

--- webpack.config.js
+++ webpack.config.js
@@ -26,16 +26,16 @@ module.exports = {
   module: {
     // 规则
     rules: [
-      // {
-      //   test: /\.(j|t)sx?$/,
-      //   use: "babel-loader",
-      //   exclude: /node_modules/,
-      // },
       {
-        test: /\.tsx?$/, // 匹配 TypeScript 文件
-        use: "ts-loader", // 使用 ts-loader 来处理
-        exclude: /node_modules/, // 排除 node_modules 文件夹
+        test: /\.(j|t)sx?$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
       },
+      // {
+      //   test: /\.tsx?$/, // 匹配 TypeScript 文件
+      //   use: "ts-loader", // 使用 ts-loader 来处理
+      //   exclude: /node_modules/, // 排除 node_modules 文件夹
+      // },
       {
         test: /\.less$/,
         use: [
